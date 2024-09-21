64daa130d8845ace82de1fe3f9c3303aa78a6942
unknown <icangramda003@gmail.com>
2024-09-21 17:04:29 +0700

    first commit

---
+++ Tugas-1/tugas-mandiri-1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf ("NAMA: Icang Ramdani\n");
+	printf ("NIM: 2441020\n");
+	printf ("JURUSAN: Teknik Informatika\n");
+	return 0;
+}
---
+++ Tugas-1/tugas-mandiri-2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int p,l,luas;
+	printf("Masukan panjang persegi : ");
+	scanf("%d", &p);
+	printf("Masukan lebar persegi : ");
+	scanf("%d", &l);
+	luas=p*l;
+	printf("Luas persegi panjang adalah %d", luas);
+	return 0;
+}
